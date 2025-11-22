21efa860cc3c3ea796b73673a012cdaa6aadf87b
Landezl <lander.zulueta@gmail.com>
2025-11-22 16:22:17 +0100

    changes

--- Fidelities and convergence/displaced-b-basis.py
+++ Fidelities and convergence/displaced-b-basis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # ----- Fixed parameters -----
 Delta = 1.0  # Qubit splitting (Δ)
-GAMMA_FIXED = 0.8 
+GAMMA_FIXED = 0.8  # Choose one γ > 1/√2 ≈ 0.707 (e.g., 1.1)
 
 # ----- ω values to compare (approaching classical oscillator limit) -----
 omega_list = np.array([0.5, 0.05, 0.005, 0.0005, 0.00005])
@@ -30,34 +30,53 @@ def truncate_spin_fock_state(psi_big, N_minus_1):
     psi_small = qt.Qobj(truncated_vec.reshape((2 * N_minus_1, 1)), dims=[[2, N_minus_1], [1, 1]])
     return psi_small.unit()
 
-# -----------------------------------------------
-# Displaced Hamiltonian H'_+
-# -----------------------------------------------
-def displaced_rabi_ground_state(N, omega, Delta, gamma):
+# -----------------------------------------------------------
+# NEW HAMILTONIAN:  Squeezed–Displaced Rabi H'_+ (from images)
+# -----------------------------------------------------------
+def displaced_squeezed_rabi_ground_state(N, omega, Delta, gamma):
     """
-    Ground state and energy of:
-    H'_+ = ω a†a + Δ σ_z + ω α (a + a†)
-           + g σ_x (a + a†) + 2 g α σ_x + ω α²
-    with α = +sqrt(γ² - 1/(4γ²)) and g = γ sqrt(ω Δ).
+    Ground state of the squeezed–displaced Hamiltonian H'_+:
+    H'_+ = ω cosh(2r) b†b 
+           + (ω/2) sinh(2r)(b^2 + b†2)
+           + e^r (b+b†)[ ω α_s + g cos(2θ) τ_x − g sin(2θ) τ_z ]
+           + Δ̃ τ_z
     """
-    a = qt.tensor(qt.qeye(2), qt.destroy(N))
-    num = a.dag() * a
-    sx = qt.tensor(qt.sigmax(), qt.qeye(N))
-    sz = qt.tensor(qt.sigmaz(), qt.qeye(N))
+
+    # Bosonic operators
+    b = qt.tensor(qt.qeye(2), qt.destroy(N))
     Id = qt.tensor(qt.qeye(2), qt.qeye(N))
+    num = b.dag() * b
 
-    # Coupling constants
+    # Spin operators
+    tx = qt.tensor(qt.sigmax(), qt.qeye(N))
+    tz = qt.tensor(qt.sigmaz(), qt.qeye(N))
+
+    # Coupling
     g = gamma * np.sqrt(omega * Delta)
-    radicand = gamma**2 - 1.0/(4.0 * gamma**2)
-    alpha = np.sqrt(max(radicand, 0.0))  # + root only
 
-    # Hamiltonian H'_+
-    H = (omega * num
-         + Delta * sz
-         + omega * alpha * (a + a.dag())
-         + g * sx * (a + a.dag())
-         + 2.0 * g * alpha * sx
-         + omega * alpha**2 * Id)
+    # α_s  (displacement)
+    alpha_s = np.sqrt(max(gamma**2 - 1/(4*gamma**2), 0))
+
+    # Δ̃  (effective qubit gap)
+    Delta_tilde = np.sqrt(Delta**2 + (2*g*alpha_s)**2)
+
+    # Squeezing parameter r
+    arg = 1 - (4 * g**2 * Delta**2) / (omega * Delta_tilde**3)
+    r = -0.25 * np.log(arg)
+
+    # Rotation angles
+    cos2θ = Delta / Delta_tilde
+    sin2θ = -(2*g*alpha_s) / Delta_tilde 
+
+    # Build H'_+
+    H = (
+        omega * np.cosh(2*r) * num
+        + (omega/2) * np.sinh(2*r) * (b**2 + b.dag()**2)
+        + np.exp(r) * (b + b.dag()) * (
+            omega*alpha_s + g*cos2θ*tx - g*sin2θ*tz
+        )
+        + Delta_tilde * tz
+    )
 
     E0, psi0 = H.groundstate()
     return float(E0), psi0
@@ -75,7 +94,7 @@ for om, g_val in param_list:
     F_vals = [np.nan]
 
     for N in N_list:
-        E0, psi_N = displaced_rabi_ground_state(N, om, Delta, GAMMA_FIXED)
+        E0, psi_N = displaced_squeezed_rabi_ground_state(N, om, Delta, GAMMA_FIXED)
         E0_vals.append(E0)
 
         if psi_prev is not None:
@@ -103,7 +122,6 @@ plt.rcParams.update({
     "lines.markersize": 5,
 })
 linestyles = ['-', '--', '-.', ':', (0, (3, 1, 1, 1, 1, 1))]
-
 
 # -----------------------------------------------
 # Figure 1: Fidelity vs N
